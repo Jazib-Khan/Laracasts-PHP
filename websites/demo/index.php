f0fef7268a46983b6d9c52c5bdaae62292b6f62e
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-04-02 19:05:01 +0100

    Conditionals and Booleans

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -3,17 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
 
-    <h1>
-        <?php 
+    <?php 
+        $name = "Notes from Underground";
+        $read = true;
 
-            $greeting = "Hey";
-            echo "$greeting young world";
+        if($read) {
+            $message = "You have read $name";
+        } else {
+            $message = "You have not read $name";
+        }
+    ?>
 
-        ?>
-    </h1>
+    <h1><?= $message ?></h1>
         
 </body>
 </html>

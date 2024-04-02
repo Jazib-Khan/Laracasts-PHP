83c278463d2438638a3b9c89b692cb77d4a54f4a
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-04-02 19:27:53 +0100

    Associative Arrays (Key value pairs)

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -3,33 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-    </style>
 </head>
 <body>
 
-    <h1>Recommended Books</h1>
-
     <?php
         $books = [
-            "Notes from Underground",
-            "Crimes and Punishment",
-            "The Brothers Karamazov",
+            [
+                "name" => "Notes from Underground",
+                "author" => "Fyodor Dostoevsky",
+                "year" => "1864",
+                "url" => "https://en.wikipedia.org/wiki/Notes_from_Underground"
+            ],
+            [
+                "name" => "Crime and Punishment",
+                "author" => "Fyodor Dostoevsky",
+                "year" => "1866",
+                "url" => "https://en.wikipedia.org/wiki/Crime_and_Punishment"
+            ],
+            [
+                "name" => "The Brothers Karamazov",
+                "author" => "Fyodor Dostoevsky",
+                "year" => "1880",
+                "url" => "https://en.wikipedia.org/wiki/The_Brothers_Karamazov"
+            ],
         ];
     ?>
 
     <ul>
         <?php foreach ($books as $book) : ?>
-            <li><?= $book ?></li>
+            <li>
+                <a href="<?= $book['url']; ?>" />
+                <?= $book['name']; ?>
+            </li>
         <?php endforeach; ?>
     </ul>
-        
+
 </body>
 </html>

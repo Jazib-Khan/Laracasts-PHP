7081fdf611a1594d18049997d56e33bbc3db67da
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-04-02 21:08:39 +0100

    Functions and filters

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -11,32 +11,54 @@
             [
                 "name" => "Notes from Underground",
                 "author" => "Fyodor Dostoevsky",
-                "year" => "1864",
+                "year" => 1864,
                 "url" => "https://en.wikipedia.org/wiki/Notes_from_Underground"
             ],
             [
                 "name" => "Crime and Punishment",
                 "author" => "Fyodor Dostoevsky",
-                "year" => "1866",
+                "year" => 1866,
                 "url" => "https://en.wikipedia.org/wiki/Crime_and_Punishment"
             ],
             [
-                "name" => "The Brothers Karamazov",
-                "author" => "Fyodor Dostoevsky",
-                "year" => "1880",
-                "url" => "https://en.wikipedia.org/wiki/The_Brothers_Karamazov"
+                "name" => "Slaughter House V",
+                "author" => "Kurt Vonnegut",
+                "year" => 1969,
+                "url" => "https://en.wikipedia.org/wiki/Slaughterhouse-Five"
+            ],
+            [
+                "name" => "Moby Dick",
+                "author" => "Herman Melville",
+                "year" => 1851,
+                "url" => "https://en.wikipedia.org/wiki/Mobdy-Dick"
             ],
         ];
+
+        function filterByAuthor($books, $author) {
+
+            $filteredBooks = [];
+
+            foreach ($books as $book) {
+                if ($book['author'] === $author) {
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+        }
+
     ?>
 
     <ul>
-        <?php foreach ($books as $book) : ?>
+        <?php foreach (filterByAuthor($books, 'Fyodor Dostoevsky') as $book) : ?>
             <li>
-                <a href="<?= $book['url']; ?>" />
-                <?= $book['name']; ?>
+                <a href="<?= $book['url']; ?>" >
+                    <?= $book['name']; ?> (<?= $book['year']; ?>) - By <?= $book['author']; ?>
+                </a>
             </li>
         <?php endforeach; ?>
     </ul>
+
 
 </body>
 </html>

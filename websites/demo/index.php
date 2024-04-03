d032ad313f56070791357bd62affc53f9de88cb4
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-04-03 18:10:31 +0100

    Page Links

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,34 +1,3 @@
 <?php
 
-$books = [
-    [
-        "name" => "Notes from Underground",
-        "author" => "Fyodor Dostoevsky",
-        "year" => 1864,
-        "url" => "https://en.wikipedia.org/wiki/Notes_from_Underground"
-    ],
-    [
-        "name" => "Crime and Punishment",
-        "author" => "Fyodor Dostoevsky",
-        "year" => 1866,
-        "url" => "https://en.wikipedia.org/wiki/Crime_and_Punishment"
-    ],
-    [
-        "name" => "Slaughter House V",
-        "author" => "Kurt Vonnegut",
-        "year" => 1969,
-        "url" => "https://en.wikipedia.org/wiki/Slaughterhouse-Five"
-    ],
-    [
-        "name" => "Moby Dick",
-        "author" => "Herman Melville",
-        "year" => 1851,
-        "url" => "https://en.wikipedia.org/wiki/Mobdy-Dick"
-    ],
-];
-
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['author'] === 'Fyodor Dostoevsky';
-});
-
-require ("index.view.php");
+require ("index.view.php"); 

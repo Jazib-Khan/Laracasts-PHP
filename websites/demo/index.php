c78fc27b7fbfc8c4b639359603a2147594c6fe6e
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-04-03 01:13:31 +0100

    Separating logic from the template

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,69 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
+<?php
 
-    <?php
-        $books = [
-            [
-                "name" => "Notes from Underground",
-                "author" => "Fyodor Dostoevsky",
-                "year" => 1864,
-                "url" => "https://en.wikipedia.org/wiki/Notes_from_Underground"
-            ],
-            [
-                "name" => "Crime and Punishment",
-                "author" => "Fyodor Dostoevsky",
-                "year" => 1866,
-                "url" => "https://en.wikipedia.org/wiki/Crime_and_Punishment"
-            ],
-            [
-                "name" => "Slaughter House V",
-                "author" => "Kurt Vonnegut",
-                "year" => 1969,
-                "url" => "https://en.wikipedia.org/wiki/Slaughterhouse-Five"
-            ],
-            [
-                "name" => "Moby Dick",
-                "author" => "Herman Melville",
-                "year" => 1851,
-                "url" => "https://en.wikipedia.org/wiki/Mobdy-Dick"
-            ],
-        ];
+$books = [
+    [
+        "name" => "Notes from Underground",
+        "author" => "Fyodor Dostoevsky",
+        "year" => 1864,
+        "url" => "https://en.wikipedia.org/wiki/Notes_from_Underground"
+    ],
+    [
+        "name" => "Crime and Punishment",
+        "author" => "Fyodor Dostoevsky",
+        "year" => 1866,
+        "url" => "https://en.wikipedia.org/wiki/Crime_and_Punishment"
+    ],
+    [
+        "name" => "Slaughter House V",
+        "author" => "Kurt Vonnegut",
+        "year" => 1969,
+        "url" => "https://en.wikipedia.org/wiki/Slaughterhouse-Five"
+    ],
+    [
+        "name" => "Moby Dick",
+        "author" => "Herman Melville",
+        "year" => 1851,
+        "url" => "https://en.wikipedia.org/wiki/Mobdy-Dick"
+    ],
+];
 
-        // function filter($items, $fn) {
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['author'] === 'Fyodor Dostoevsky';
+});
 
-        //     $filteredItems = [];
-
-        //     foreach ($items as $item) {
-        //         if ($fn($item)) {
-        //             $filteredItems[] = $item;
-        //         }
-        //     }
-
-        //     return $filteredItems;
-        // }
-
-
-        $filteredBooks = array_filter($books, function ($book) {
-            return $book['author'] === 'Fyodor Dostoevsky';
-        });
-
-    ?>
-
-    <ul> 
-        <?php foreach ($filteredBooks as $book) : ?>
-            <li>
-                <a href="<?= $book['url']; ?>" >
-                    <?= $book['name']; ?> (<?= $book['year']; ?>) - By <?= $book['author']; ?>
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-
-
-</body>
-</html>
+require ("index.view.php");

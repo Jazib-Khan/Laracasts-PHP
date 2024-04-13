8dc260e0364c721d46308473a9d6ebe29d2b28d1
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-04-13 01:55:37 +0100

    Autoloading and Extraction

--- websites/demo/functions.php
+++ websites/demo/functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function dd($value) {
+function dd($value) 
+{
     
     echo "<pre>";
     var_dump($value);
@@ -9,12 +10,26 @@ function dd($value) {
     die();    
 }
 
-function urlIs($value) {
+function urlIs($value) 
+{
     return $_SERVER['REQUEST_URI'] === $value ? 'bg-gray-900 text-white' : 'text-gray-300';
 }
 
-function authorize($condition, $statusCode = Response::FORBIDDEN) {
+function authorize($condition, $statusCode = Response::FORBIDDEN) 
+{
     if (! $condition) {
         abort($statusCode);
     }
+}
+
+function base_path($path)
+{ 
+    return BASE_PATH . $path;
+}
+
+function view($path, $attributes = [])
+{ 
+    extract($attributes);
+
+    require base_path('views/' . $path);
 }

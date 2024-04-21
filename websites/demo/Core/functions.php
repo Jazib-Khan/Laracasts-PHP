83b29115cf71bff8646dd41c45dc684fd64f1888
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-04-21 18:03:14 +0100

    Built a better router

--- websites/demo/Core/functions.php
+++ websites/demo/Core/functions.php
@@ -17,10 +17,19 @@ function urlIs($value)
     return $_SERVER['REQUEST_URI'] === $value ? 'bg-gray-900 text-white' : 'text-gray-300';
 }
 
-function authorize($condition, $statusCode = Response::FORBIDDEN) 
+function abort($code = 404) 
+{
+    http_response_code($code);
+    
+    require base_path("views/{$code}.php");
+    
+    die();
+}
+
+function authorize($condition, $status = Response::FORBIDDEN) 
 {
     if (! $condition) {
-        abort($statusCode);
+        abort($status);
     }
 }
 
--- websites/demo/views/notes/show.view.php
+++ websites/demo/views/notes/show.view.php
@@ -14,6 +14,7 @@
         <p><?= htmlspecialchars($note['body']) ?></p>    
 
         <form class="mt-6" method="POST">
+            <input type="hidden" name="_method" value="DELETE">
             <input type="hidden" name="id" value="<?= $note['id'] ?>">
             <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4">Delete</button>
         </form>

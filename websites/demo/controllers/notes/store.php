75ca6a92c29f63b6d9072f5fc939defa50c9380b
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-04-22 15:09:00 +0100

    Service container for db connection

--- websites/demo/controllers/notes/store.php
+++ websites/demo/controllers/notes/store.php
@@ -1,12 +1,12 @@
 <?php
 
+use Core\App;
 use Core\Database;
 use Core\Validator; 
 
-$errors = [];
+$db = App::resolve(Database::class);
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+$errors = [];
 
 if(! Validator::string($_POST['body'], 1, 1000)) {
     $errors['body'] = 'A body of no more than 1,000 characters is required';

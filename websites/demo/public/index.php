2e732e0e24d3dab728fe957ab4a538731c7f5b4b
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-06-14 17:50:23 +0100

    Automatically redirect back upon validation

--- websites/demo/public/index.php
+++ websites/demo/public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Session;
+use Core\ValidationException;
 
 session_start();
 
@@ -9,7 +10,6 @@ const BASE_PATH = __DIR__.'/../';
 require BASE_PATH.'Core/functions.php';
 
 spl_autoload_register(function ($class) {
-
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
     require base_path("{$class}.php");
@@ -18,12 +18,19 @@ spl_autoload_register(function ($class) {
 require base_path('bootstrap.php');
 
 $router = new \Core\Router();
-
 $routes = require base_path('routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
-$router->route($uri, $method);
+try {
+    $router->route($uri, $method);
+} catch (ValidationException $exception) {
+    Session::flash('errors', $exception->errors);
+    Session::flash('old', $exception->old);
+
+    return redirect($router->previousUrl());
+}
+
 
 Session::unflash();
